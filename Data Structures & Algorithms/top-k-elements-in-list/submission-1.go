603ac43d627996215fc829853b22a945d2bacd58
Michael Ko <gigaspyder@gmail.com>
2026-08-11 15:14:18 -0700

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.go
@@ -0,0 +1,29 @@
+func topKFrequent(nums []int, k int) []int {
+	freq := make(map[int]int)
+
+	for _, n := range nums {
+		freq[n]++
+	}
+
+	freqList := make([][]int, len(freq))
+	idx := 0
+	for k,v := range freq {
+		freqList[idx] = []int{k,v}
+		idx++
+	}
+
+	// probably should use a real sort algorithm here (I can't write any by heart)
+	for i := 0; i < len(freqList); i++ {
+		for j := i+1; j < len(freqList); j++ {
+			if freqList[i][1] < freqList[j][1] {
+				freqList[i], freqList[j] = freqList[j], freqList[i]
+			}
+		}
+	}
+
+	largest := make([]int, k)
+	for i := range k {
+		largest[i] = freqList[i][0]
+	}
+	return largest
+}
